454ea106ca387275da733eb07be5043c83a09c18
b2905607825@163.com <b2905607825@163.com>
2026-09-11 09:25:39 +0800

    chore: add missing hotkey hint

--- plugin/mini.bufremove.lua
+++ plugin/mini.bufremove.lua
@@ -1,1 +1,10 @@
 vim.pack.add({ 'https://github.com/nvim-mini/mini.bufremove' })
+
+local bufremove = require('mini.bufremove')
+
+vim.keymap.set('n', '<leader>bd', function()
+  bufremove.delete()
+end, { desc = '关闭当前缓冲区' })
+vim.keymap.set('n', '<leader>bw', function()
+  bufremove.wipeout()
+end, { desc = '彻底关闭当前缓冲区' })
